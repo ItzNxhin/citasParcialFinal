876ffb479414ef033950503500395629f1125ad6
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-05-27 08:57:22 -0500

    first commit

---
+++ src/test/java/citas/citas/CitasApplicationTests.java
@@ -0,0 +1,13 @@
+package citas.citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

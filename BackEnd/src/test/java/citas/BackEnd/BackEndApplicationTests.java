8f1153d5be9d9a6f56f0b3c65e0c948ed723c407
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-05-31 21:01:01 -0500

    Correcion proyecto

--- BackEnd/src/test/java/citas/BackEnd/BackEndApplicationTests.java
+++ BackEnd/src/test/java/citas/BackEnd/BackEndApplicationTests.java
@@ -1,10 +1,10 @@
-package citas.citas;
+package citas.BackEnd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CitasApplicationTests {
+class BackEndApplicationTests {
 
 	@Test
 	void contextLoads() {
